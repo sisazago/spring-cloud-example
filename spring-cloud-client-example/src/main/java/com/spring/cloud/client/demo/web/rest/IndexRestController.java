f43f4ec5d2da2ea91b54b73457c76de219d1d33e
Sebastian Isaza Gomez <isazagomez@gmail.com>
2016-10-20 16:00:07 -0500

    Spring cloud example

--- spring-cloud-client-example/src/main/java/com/spring/cloud/client/demo/web/rest/IndexRestController.java
+++ spring-cloud-client-example/src/main/java/com/spring/cloud/client/demo/web/rest/IndexRestController.java
@@ -1,21 +1,24 @@
 package com.spring.cloud.client.demo.web.rest;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RefreshScope
 public class IndexRestController {
 	
 	@Value("${user.role}")
     private String role;
 	
-	@RequestMapping( value = "whoami/{username}", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
-	public String whoami(@PathVariable("username") String username) {
+	@Value("${user.name}")
+	private String username;
+	
+	@RequestMapping( value = "/developer", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+	public String whoami() {
 		return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
 	}
 }
